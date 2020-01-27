afea8733d8c519c38c34b18932bc91a9245e713b
Manuel David Garcia <mdgarcia@dapda.com>
2020-01-27 15:48:56 +0100

    Added server

--- quick_node_express/README.md
+++ quick_node_express/README.md
@@ -1,1 +1,14 @@
-# Quick Node + Express stuff
+# EXPRESS TEMPERATURE API
+
+**USE:**
+
+    * npm install
+    * npm start
+
+**EXAMPLE**
+
+`    curl -X POST \
+  -k http://localhost:4000/temperature \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  -d '{"temperature":"22"}'`
--- quick_node_express/server.js
+++ quick_node_express/server.js
@@ -8,27 +8,19 @@ const port = 4000;
 
 // parse JSON (application/json content-type)
 server.use(body_parser.json());
+server.engine('html', require('ejs').renderFile);
+server.set('view engine', 'ejs');
+server.set('views', __dirname);
 
 server.get("/", (req, res) => {
-   res.sendFile(__dirname + '/index.html');
+   res.render(__dirname + '/index.html', {data: data});
 });
 
-server.get("/items", (req, res) => {
+server.get("/temperature", (req, res) => {
    res.json(data);
 });
 
-server.get("/items/:id", (req, res) => {
-   const itemId = req.params.id;
-   const item = data.find(_item => _item.id === itemId);
-
-   if (item) {
-      res.json(item);
-   } else {
-      res.json({ message: `item ${itemId} doesn't exist` })
-   }
-});
-
-server.post("/items", (req, res) => {
+server.post("/temperature", (req, res) => {
    const item = req.body;
    console.log('Adding new item: ', item);
 
@@ -36,44 +28,6 @@ server.post("/items", (req, res) => {
    data.push(item)
 
    // return updated list
-   res.json(data);
-});
-
-// update an item
-server.put("/items/:id", (req, res) => {
-   const itemId = req.params.id;
-   const item = req.body;
-   console.log("Editing item: ", itemId, " to be ", item);
-
-   const updatedListItems = [];
-   // loop through list to find and replace one item
-   data.forEach(oldItem => {
-      if (oldItem.id === itemId) {
-         updatedListItems.push(item);
-      } else {
-         updatedListItems.push(oldItem);
-      }
-   });
-
-   // replace old list with new one
-   data = updatedListItems;
-
-   res.json(data);
-});
-
-// delete item from list
-// delete item from list
-server.delete("/items/:id", (req, res) => {
-   const itemId = req.params.id;
-
-   console.log("Delete item with id: ", itemId);
-
-   // filter list copy, by excluding item to delete
-   const filtered_list = data.filter(item => item.id !== itemId);
-
-   // replace old list with new one
-   data = filtered_list;
-
    res.json(data);
 });
 
